ad0ed2aee90d8de49bda1fc0bb7d1a8a1ba5c9bc
Jensey <50929863+seyleigh@users.noreply.github.com>
2019-12-11 18:10:15 -0500

    mongo testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // import { Mongoose } from "mongoose"
 var mongoose = require("mongoose");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds253368.mlab.com:53368/heroku_11dr059t";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://usertest:thatsnotit3x@ds253368.mlab.com:53368/heroku_11dr059t";
 
 mongoose.connect(MONGODB_URI);
